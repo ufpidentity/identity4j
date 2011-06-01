5b28b4d1dc7f7628dc5413a80727383a0548b4cd
Richard Levenberg <richardl@ufp.com>
2011-06-01 11:00:09 -0700

    unit test

---
+++ src/test/java/com/ufp/identity4j/test/TestIdentity4J.java
@@ -0,0 +1,71 @@
+package com.ufp.identity4j.test;
+
+import java.io.File;
+
+import java.util.Map;
+import java.util.HashMap;
+
+import org.junit.Test;
+import org.junit.Before;
+import static org.junit.Assert.*;
+
+import com.ufp.identity4j.data.AuthenticationContext;
+import com.ufp.identity4j.data.AuthenticationPretext;
+import com.ufp.identity4j.data.DisplayItem;
+
+import com.ufp.identity4j.provider.IdentityServiceProvider;
+import com.ufp.identity4j.truststore.KeyManagerFactoryBuilder;
+import com.ufp.identity4j.truststore.TrustManagerFactoryBuilder;
+import com.ufp.identity4j.truststore.LocalHostnameVerifier;
+import com.ufp.identity4j.resolver.LocalhostIdentityResolver;
+
+public class TestIdentity4J {
+    private IdentityServiceProvider identityServiceProvider = new IdentityServiceProvider();
+
+    @Before 
+    public void setupIdentity4JProvider() throws Exception {
+        identityServiceProvider = new IdentityServiceProvider();
+
+        // setup the key manager factory
+        KeyManagerFactoryBuilder keyManagerFactoryBuilder = new KeyManagerFactoryBuilder();
+        keyManagerFactoryBuilder.setStore(new File("src/test/resources/example.com.p12"));
+        keyManagerFactoryBuilder.setPassphrase("example123");
+
+        // setup the trust store
+        TrustManagerFactoryBuilder trustManagerFactoryBuilder = new TrustManagerFactoryBuilder();
+        trustManagerFactoryBuilder.setStore(new File("src/test/resources/truststore.jks"));
+        trustManagerFactoryBuilder.setPassphrase("pSnHa(3QDixmi%\\");
+
+        // set provider properties
+        identityServiceProvider.setKeyManagerFactoryBuilder(keyManagerFactoryBuilder);
+        identityServiceProvider.setTrustManagerFactoryBuilder(trustManagerFactoryBuilder);
+        identityServiceProvider.setHostnameVerifier(new LocalHostnameVerifier());
+        identityServiceProvider.setIdentityResolver(new LocalhostIdentityResolver());
+        identityServiceProvider.afterPropertiesSet();
+    }
+
+    @Test
+    public void TestAuthenticate() throws Exception {
+        AuthenticationPretext authenticationPretext = identityServiceProvider.preAuthenticate("guest", "example.com");
+        assertNotNull(authenticationPretext);
+        assertEquals(authenticationPretext.getResult().getValue(), "SUCCESS");
+
+        DisplayItem displayItem = authenticationPretext.getDisplayItem().get(0);
+        Map<String, String []> parameterMap = new HashMap<String, String []>();
+
+        parameterMap.put(displayItem.getName(), new String [] {"guest"});
+        AuthenticationContext authenticationContext = (AuthenticationContext)identityServiceProvider.authenticate(authenticationPretext.getName(), "example.com", parameterMap);
+        assertNotNull(authenticationContext);
+        assertEquals(authenticationContext.getResult().getValue(), "SUCCESS");
+    }
+
+    @Test
+    public void TestPreEnroll() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void TestEnroll() {
+        assertTrue(true);
+    }
+}
